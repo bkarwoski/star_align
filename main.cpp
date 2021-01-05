31437098715b4bb0980f60d7ab1ddab6777b7816
Blake Karwoski <31117797+bkarwoski@users.noreply.github.com>
2021-01-05 16:38:53 -0600

    trying to fix cmake opencv tracking missing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
-#include <iostream>
+#include <assert.h>
 #include <ceres/ceres.h>
-#include <opencv2/calib3d.hpp>
+#include <experimental/filesystem>
+#include <fstream>
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/tracking.hpp>
+#include <string>
 
-int main() {
-    std::cout << "hello world" << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cout << "Usage: ./main </path/to/images>" << std::endl;
+    exit(1);
+  }
+  std::string path = argv[1];
+  cv::Mat img = cv::imread(path);
+  if (!cv::imwrite("./output.jpg", img)) {
+    std::cerr << "could not write image" << std::endl;
+  }
+
+  std::cout << "hello world" << std::endl;
+  return 0;
 }
+// /home/blake/Pictures/Sand_Dune_Stars/time_lapse/jpg/DSC07974.JPG
